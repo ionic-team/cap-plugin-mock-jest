942acf6426c328e2860ed45874f075032cfd800c
Ken Sodemann <ken@ionic.io>
2021-07-26 10:23:19 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'demo-cap-plugin-mock-vue',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
